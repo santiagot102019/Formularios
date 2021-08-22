efa6d52814b8e23292207f284761d3a570df0f6b
santiagot102019 <santiagot102019@gmail.com>
2021-08-21 20:10:40 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css';
 
 export default function App() {
   const [inputText, setInputText] = useState('');
+  const [textareaText, setTextareaText] = useState('');
   const [selectNum, setSelectNum] = useState(3);
   const [valorMostrar, setValorMostrar] = useState('');
 
@@ -17,13 +18,21 @@ export default function App() {
     setSelectNum(valor);
   };
 
+  const handleTextarea = e => {
+    const valor = e.target.value;
+    setTextareaText(valor);
+  };
+
   return (
     <section>
       <h2>Formulario</h2>
       <form>
+        <h4>Input</h4>
         <label>
           <input type="text" value={inputText} onChange={handleInput} />
         </label>
+        <hr />
+        <h4>Select</h4>
         <select value={selectNum} onChange={handleSelect}>
           <option value="">Seleccionar</option>
           <option value="1">Uno</option>
@@ -33,7 +42,8 @@ export default function App() {
           <option value="5">Cinco</option>
           <option value="6">Seis</option>
         </select>
-
+        <hr />
+        <h4>Select Multiple</h4>
         <select multiple={true} value={[1, 2]}>
           <option value="">Seleccionar</option>
           <option value="1">Uno</option>
@@ -43,9 +53,19 @@ export default function App() {
           <option value="5">Cinco</option>
           <option value="6">Seis</option>
         </select>
+        <hr />
+        <h4>Textarea</h4>
+        <label>
+          <textarea
+            cols="30"
+            rows="10"
+            value={textareaText}
+            onChange={handleTextarea}
+          />
+        </label>
       </form>
-      <br />
-      Resultado: <span>{valorMostrar}</span>
+      <hr />
+      Resultado Input: <span>{valorMostrar}</span>
     </section>
   );
 }
